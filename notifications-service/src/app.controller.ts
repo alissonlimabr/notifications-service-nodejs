e3507ebde66cd947d166ece721a4fd62fb4287b5
Alisson Lima <amlxd5@gmail.com>
2022-12-16 22:04:23 -0500

    Modelagem do Schema de notificações

--- notifications-service/src/app.controller.ts
+++ notifications-service/src/app.controller.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
+  @Get('home') // decorator para mapear a url (semelhante ao @GetMapping do Springboot)
   getHello(): string {
     return this.appService.getHello();
   }
 }
+
+/*
+                            ----------------------------------
+                            Inversão x injeção de dependências
+                            ----------------------------------
+
+  Injeção = é uma forma de automatizar as dependências no momento em que as classes forem instanciadas (dependências pré-determinadas).
+
+  Inversão = quem instancia determinada classe, vai dizer quais as dependências que pertencem a ela.
+
+*/
